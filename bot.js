0a8e1e9e89982bb89dab623ffbdd7aabdd5d6c92
Henrique Bustillos <henrique.ssbl@hotmail.com>
2021-04-20 23:02:36 -0300

    removido setActivity

--- bot.js
+++ bot.js
@@ -11,12 +11,10 @@ client.on("ready", () => {
 
 client.on("guildCreate", guild => {
   console.log(`O bot entrou no servidor: ${guild.name} (id: ${guild.id}). População: ${guild.memberCount} membros! `);
-  client.user.setActivity(`Estou em ${client.guilds.cache.size} servidores`);
 });
 
 client.on("guildDelete", guild => {
   console.log(`O bot foi removido do servidor ${guild.name} (id: ${guild.id})`);
-  client.user.setActivity(`Servindo ${client.guilds.cache.size} servidor.`)
 })
 
 client.on("message", async message => {
